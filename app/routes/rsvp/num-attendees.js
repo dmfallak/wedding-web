858d47296cd7753dbe58e76d38762f19e7fa61ab
Devin Fallak <dmfallak@gmail.com>
2016-12-12 20:25:35 -0500

    Validation works

--- app/routes/rsvp/num-attendees.js
+++ app/routes/rsvp/num-attendees.js
@@ -3,5 +3,20 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('guest', params.guest_id);
+  },
+
+  /* globals $ */
+
+  actions: {
+    next: function() {
+      var num = this.currentModel.get("attendingNum");
+      var max = this.currentModel.get("attendingMax");
+
+      if (num < 1 || num > max) {
+        $("#attendingNumGroup").addClass("has-error");
+        $("#helpBlock").css("display", "inline");
+      }
+      console.log("attendingNum is "+num);
+    }
   }
 });
