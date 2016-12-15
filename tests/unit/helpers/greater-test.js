64138032321414957c7dc3b2987debbb916e7de8
Devin Fallak <dmfallak@gmail.com>
2016-12-14 21:56:37 -0500

    Adds logic for disabling shuttle choices for full shuttles

---
+++ tests/unit/helpers/greater-test.js
@@ -0,0 +1,12 @@
+
+import { greater } from 'wedding/helpers/greater';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | greater');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = greater([42]);
+  assert.ok(result);
+});
+
