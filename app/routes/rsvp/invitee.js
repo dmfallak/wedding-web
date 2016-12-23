d9496700de922e8853a0af833e0240e6a17933ac
Devin Fallak <dmfallak@gmail.com>
2016-12-22 20:50:55 -0500

    Adds validation to invitee input

--- app/routes/rsvp/invitee.js
+++ app/routes/rsvp/invitee.js
@@ -4,15 +4,24 @@ export default Ember.Route.extend({
   model() {
   },
 
+  /* globals $ */
   actions: {
     submitInvitee: function(invitee) {
       var store = this.get("store");
       var route = this;
       store.queryRecord('guest', {invitee: invitee}).then(function(guest) {
+        $("#inviteeGroup").removeClass("has-error");
+        $("#helpBlock").css("display", "none");
+
         if (guest.get('responded')) {
           route.transitionTo('rsvp.already_responded');
         } else {
           route.transitionTo('rsvp.attending', guest);
+        }
+      }, function(reason){
+        if (reason.errors[0].status === "404") {
+          $("#inviteeGroup").addClass("has-error");
+          $("#helpBlock").css("display", "inline");
         }
       });
     }
